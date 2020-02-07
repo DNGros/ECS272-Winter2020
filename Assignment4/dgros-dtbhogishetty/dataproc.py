ae1ef790702315f643946dd75f591cd00853eee6
David Gros <dngros@live.com>
2020-02-07 12:42:23 -0800

    starter files for a4

---
+++ Assignment4/dgros-dtbhogishetty/dataproc.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+if __name__ == "__main__":
+    # Make sure we can load the data
+    # Hello dash
+    # Dim reduction
+    #   Map the pokemon to a vector
+    #   Run dim reduction
+    #   Show in dash
+    #   Paramertized dim reduction
+    #       Show the multi selection
+    #       Redo dim reduction when changing the parameters
+    # Show stats bar graph
+    # Figure out how to link stats bargraph and scatter plot of dim reduction
+    # Be able to show table of things in given filter
+    # Sankey
+    #   Draw the sankey
+    #   Extra interaction??
+    # (Optional) figure out picture in table
+    pass
---
+++ Assignment4/dgros-dtbhogishetty/main.py
@@ -0,0 +1,12 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+
+
+
+
+if __name__ == "__main__":
+    pass
